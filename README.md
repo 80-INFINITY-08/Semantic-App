7d85e7be8a184720fca27bb9d0160415942d2aa6
Dibya_Prasetya <dbyapras08@gmail.com>
2025-11-09 14:53:51 +0700

    tambah nama anggota kelompok

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Semantic-App
+
+## Dibuat oleh :
+
+- 202251199 - Dibya Agung Prasetya
+- 202251208 - Rifqi Fatekhatus Sofyan
+- 202251186 - Sukron Aji Khasanatan
+- 202251191 - Ferdy Hasan
--- app.py
+++ app.py
@@ -4,3 +4,10 @@ st.set_page_config(page_title="Semantic Web Ontologies", layout="wide")
 
 st.title("🌐 Semantic Web Ontologies")
 st.write("Pilih halaman di sidebar untuk melihat ontologi dan CQ masing-masing tema.")
+st.subheader("Dibuat oleh :")
+st.markdown("""
+1. 202251199 - Dibya Agung Prasetya
+2. 202251208 - Rifqi Fatekhatus Sofyan
+3. 202251186 - Sukron Aji Khasanatan
+4. 202251191 - Ferdy Hasan
+""")
